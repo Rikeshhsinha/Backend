9aceb1ee04416351718f828c82a4feac48d7c91b
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-08-27 23:45:57 +0530

    Add async handler

---
+++ utils/asyncHandler.js
@@ -0,0 +1,21 @@
+// const asyncHandler () => {}
+
+
+
+
+
+export {asyncHandler}
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(res,req,next);
+//     }
+//     catch (error) {
+//          res.status(error.code || 500).json({
+//         success: false,
+//         message: error.message
+//          });
+// }
+// }
